b5df59dbd3ed8409468f5c73bc99e74e97f7d051
Matt Burns <matthewburns@gmail.com>
2013-10-27 13:00:53 -0700

    successful post simulation checking project out from github and pushing it to heroku

--- src/dmgr/core.clj
+++ src/dmgr/core.clj
@@ -7,7 +7,17 @@
             [compojure.handler :as handler]
             [clojure.java.io :as io]
             [clojure.string :as str]
-            [conch.core])
+            [conch.core]
+            [clj-jgit.porcelain :refer [with-identity]]
+            [fs.core :refer [delete-dir]]
+            [tentacles.repos :as github :refer [archive-link ]])
+ (:import [java.io FileNotFoundException File]
+          [org.eclipse.jgit.lib RepositoryBuilder ]
+          [org.eclipse.jgit.internal.storage.file FileRepository]
+           [org.eclipse.jgit.api Git InitCommand StatusCommand AddCommand
+            ListBranchCommand PullCommand MergeCommand LogCommand
+            LsRemoteCommand Status ResetCommand$ResetType
+            FetchCommand])
   (:gen-class))
 
 (defn expand-path [path] (.getCanonicalPath (java.io.File. path)))
@@ -17,15 +27,6 @@
 (defn get-github-repo-uri [app-name & [uri]] (if uri  uri (env (format "%s_GITHUB_REPO" app-name))))
 (defn get-heroku-repo-uri [app-name & [uri]]   (if  uri uri (env (format "%s_HEROKU_REPO" app-name))))
 (defn get-ssh-private-key [app-name & [private-key]]  (if private-key private-key (env (format "%s_SSH_KEY" app-name))))
-
-(defn get-deployment-parameters
-  "Convience function to assemble the parameters also make testing easier"
-  [app-name & [opts]]
-  {:app-name app-name
-   :github-repo-uri (get-github-repo-uri app-name (:github-repo-uri opts))
-   :heroku-repo-uri (get-heroku-repo-uri app-name (:heroku-repo-uri opts))
-   :ssh-private-key (get-ssh-private-key app-name (:ssh-private-key opts))
-   :access-key (get-access-key (:access-key opts))})
 
 (defn run
   "Run the shell commands with the correct environment variables returning exit status code"
@@ -39,46 +40,50 @@
     (future (conch.core/stream-to :err proc *out*))
     (conch.core/exit-code proc)))
 
-
-
-(defn git-deploy-cmds
-  "Creates the shell commands"
-  [{:keys [app-name github-repo-uri heroku-repo-uri] :as args}]
-  {:fetch-and-reset  (format   "cd  %s/repo && git fetch && git reset --hard origin/master" app-name)
-   :clone (format "cd %s  && git clone https://%s repo" app-name  github-repo-uri)
-   ;;   :push-to-heroku (format "cd %s/repo && heroku git:remote -a %s && git push heroku master" app-name app-name app-name  heroku-repo-uri)
-   :push-to-heroku (format "cd %s/repo && git remote add heroku git@heroku.com:%s.git &&  git config heroku.remote heroku && git push heroku master"
-                           app-name app-name)
-   ;:remove-staging-dir (format "rm -rf %s" app-name)
-   })
-
-:push-to-heroku
-;;   (run app-path (str "cd " app "/repo && bundle install --without=production && bundle exec jekyll build && git add . && git commit -am 'auto-build update' && git push -f " (env (str app "_HEROKU_REPO")) " master"))
+(defn git-push-heroku-master
+  "Push the project into heroku to trigger its build deploy"
+  [{:keys [app-name staging-parent-dir-path src-repo-uri src-repo-remote branch-name] :as args} ]
+  (let [app-path (expand-path (format "%s/staging" staging-parent-dir-path))
+        staging-dir (File. app-path)
+        _ (when (.isDirectory staging-dir) (delete-dir staging-dir))
+        from-git (-> (Git/cloneRepository) (.setURI src-repo-uri) (.setDirectory staging-dir)
+                     (.setBranch branch-name) (.setBare false) (.setRemote src-repo-remote)
+                     (.setNoCheckout false) .call)
+        from-repo (.getRepository from-git)
+        repo-config (.getConfig from-repo)
+        _ (.setString repo-config "remote" "heroku"  "url" (format "git@heroku.com:%s.git" app-name))
+        _ (.setString repo-config "remote" "heroku" "fetch" "+refs/heads/*:refs/remotes/heroku/*")
+        _ (.save repo-config)
+        commit-result (-> from-git .commit (.setMessage "deployment commit to allow a push to heroku repo master branch") .call)
+        shell-push-cmd (format "cd %s  && git push -f heroku master" app-path)]
+    (run app-path shell-push-cmd)))
 
 (defn prepare-staging-area
-  "Create a staging area"
-  [{:keys [app-name app-path ssh-private-key] :as args}]
-  (when-not (.isDirectory (io/file app-name))
-            (.mkdir (io/file app-name))
+  "Create a staging area for the app"
+  [{:keys [app-path ssh-private-key] :as args}]
+  (when-not (.isDirectory (io/file app-path))
+            (.mkdir (io/file app-path))
             (.mkdir (io/file (str app-path "/.ssh")))
             (spit (str app-path "/.ssh/id_rsa") ssh-private-key)))
 
-
 (defn deploy
-  "Creates and executes the respect git shell commands to update or initially clone a repo
-   to a staging directory.  From this staging directory deploy the application to heroku"
-  [{:keys [app-name  github-repo-uri heroku-repo-uri access-key ssh-private-key] :as args} ]
-  (let [app-path (expand-path app-name)
-        dir-present? (.isDirectory (io/file (str app-name "/repo")))
-        cmds (git-deploy-cmds args)
-        _ (prepare-staging-area (assoc args :app-path app-path))
-        error? (if dir-present?
-                 (run app-path (:fetch-and-reset cmds)) ;"Starting repo update"
-                 (run app-path (:clone cmds)))]
-    (do (run app-path (:push-to-heroku cmds))
-;;        (run app-path (:remove-staging-dir cmds))
-        )))
+  "Potential interface function"
+  [{:keys [app-name  github-repo-uri heroku-repo-uri access-key ssh-private-key
+           staging-parent-dir-path] :as args} ]
+  (let [app-path (expand-path (format "staging/%s" app-name))
+        dir-present? (.isDirectory (io/file app-path))
+        _ (prepare-staging-area (assoc args :app-path app-path))]
+    (git-push-heroku-master {:app-name app-name :staging-parent-dir-path  staging-parent-dir-path
+                             :src-repo-uri github-repo-uri :src-repo-remote "origin" :branch-name "master"})))
 
+(defn get-deployment-parameters
+  "Convience function to assemble the parameters also make testing easier"
+  [app-name & [opts]]
+  {:app-name app-name
+   :github-repo-uri (get-github-repo-uri app-name (:github-repo-uri opts))
+   :heroku-repo-uri (get-heroku-repo-uri app-name (:heroku-repo-uri opts))
+   :ssh-private-key (get-ssh-private-key app-name (:ssh-private-key opts))
+   :access-key (get-access-key (:access-key opts))})
 
 (defroutes main-routes
   (GET "/" [] "<h1>Nothing to see here, move along</h1>")
@@ -92,4 +97,5 @@
 (defn start [web-application port]
   (run-jetty web-application {:port port :join? false}))
 
-(defn -main [& port] (start app (Integer/parseInt (or (System/getenv "PORT") "8087"))))
+(defn -main [& port]
+  (start app (Integer/parseInt (or (System/getenv "PORT") "8087"))))
--- test/dmgr/test/core.clj
+++ test/dmgr/test/core.clj
@@ -3,12 +3,22 @@
     :doc "Acceptance test revolving around proving a user can trigger a heroku app install from a github webhook
          using a third heroku app running  in the clould"}
   (:use  [dmgr.core])
-  (:require [clj-webdriver.taxi :as scraper :refer [set-driver! to click exists? input-text submit quit]]
+ (:import [java.io FileNotFoundException File]
+          [org.eclipse.jgit.lib RepositoryBuilder]
+          [org.eclipse.jgit.internal.storage.file FileRepository]
+          [org.eclipse.jgit.transport RemoteConfig RefSpec URIish UsernamePasswordCredentialsProvider]
+           [org.eclipse.jgit.api Git InitCommand StatusCommand AddCommand
+            ListBranchCommand PullCommand MergeCommand LogCommand PushCommand
+            LsRemoteCommand Status ResetCommand$ResetType FetchCommand])
+ (:require [clj-webdriver.taxi :as scraper :refer [set-driver! to click exists? input-text submit quit]]
+           [clj-jgit.porcelain :refer [with-identity]]
             [clojure.core]
             [clojure.java.io :as io]
             [clojure.string :as str]
             [conch.core]
             [expectations :refer [expect]]
+
+            [fs.core :refer [delete-dir]]
             [lstoll.utils :refer [env]]
             [ring.mock.request :refer [request]]
             [tentacles.repos :refer [create-repo]]
@@ -19,10 +29,8 @@
 ;;=========================================================================
 (def test-opts {:github-repo-uri "github.com/mateoconfeugo/lhg.git"
                 :heroku-repo-uri "git@heroku.com"
-                :ssh-private-key "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDxhl4HNWTmtANOfHDjFJvZsCv2zQ53dNXpQvR5hbyIdWA6X7bhTYOhrOlAuOBp55wR4vwbRg6zQ9AhMeCAuuh4ZjO5K68gyxRtNgCEYu/TIiKq5ob2BGT1KlczqD4LpvQVnvHyH17U+aCFhO8Xl35jZ3GMr2YZc9mxh8iNqjUUUZsj0IotfQA3bzTPC3ecM+m4H4sQ0k+pUvhdB3SAvE/bM6nbRgwzPHhp5YI/oMEpIhls8TAyo3iVR6YkbHEcMvbrHUOArVoSa1uHVkoWdn0ZH/5IG7Y6msY9/QMgJmwiBvH5qKWLomtZ8vCdtn13NlSli7Ek6+PE1dktCxJudjst matthewburns@mattsmac.local"
+                :ssh-public-key "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDxhl4HNWTmtANOfHDjFJvZsCv2zQ53dNXpQvR5hbyIdWA6X7bhTYOhrOlAuOBp55wR4vwbRg6zQ9AhMeCAuuh4ZjO5K68gyxRtNgCEYu/TIiKq5ob2BGT1KlczqD4LpvQVnvHyH17U+aCFhO8Xl35jZ3GMr2YZc9mxh8iNqjUUUZsj0IotfQA3bzTPC3ecM+m4H4sQ0k+pUvhdB3SAvE/bM6nbRgwzPHhp5YI/oMEpIhls8TAyo3iVR6YkbHEcMvbrHUOArVoSa1uHVkoWdn0ZH/5IG7Y6msY9/QMgJmwiBvH5qKWLomtZ8vCdtn13NlSli7Ek6+PE1dktCxJudjst matthewburns@mattsmac.local"
                 :access-key "test123"})
-
-
 
 ;;=========================================================================
 ;; UNIT TEST:  Get the correct path
@@ -36,6 +44,8 @@
 (def test-git-deploy-cmds (git-deploy-cmds test-params))
 (def test-stageing-area (prepare-staging-area (assoc test-params :app-path (expand-path test-app-name))))
 
+(get-deployment-parameters )
+
 ;;=========================================================================
 ;; UNIT TEST:  Make sure the external process runs the proper shell commands
 ;;=========================================================================
@@ -47,11 +57,16 @@
 ;;========================================================================
 (def test-webhook-uri "/deploy?app=lhg&key=test123")
 
-(get-github-repo-uri "lhg" )
-(get-heroku-repo-uri "lhg" )
-(get-ssh-private-key "lhg")
+(def github-repo-uri (get-github-repo-uri "lhg"))
+(def heroku-repo-uri  (get-heroku-repo-uri "lhg"))
+(def private-key (get-ssh-private-key "lhg"))
 
+(git-push-heroku-master {:app-name "lhg"
+                         :staging-parent-dir-path "test"
+                         :src-repo-uri "https://github.com/mateoconfeugo/lhg.git"
+                         :src-repo-remote "origin"
+                         :branch-name "master"})
 
-;;(expect true (= (:status (app (request :post test-webhook-uri))) 200))
-
-;;(keys (System/getenv ))
+(expect true (= (:status (app (request :post test-webhook-uri))) 200))
+(def working-dir (File. (:working-copy-tmp-dir test-remote-git)))
+(delete-dir working-dir)
